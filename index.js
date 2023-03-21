c24acbb0adcb3c29eeef9d5a4f39a7a0bf319b38
joao <joao-morrudo@hotmail.com.br>
2023-03-21 20:26:37 -0300

    Comeco do projeto

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function(){
+    console.log("Servidor no ar - Porta: 3000!")
+});
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/views/home.html');
+});

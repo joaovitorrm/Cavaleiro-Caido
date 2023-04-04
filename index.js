1e187d2688cc08ed9e038e2975ba15207d03577a
JOAO VITOR RODRIGUES MORRUDO <10160145@lab-412--09.RESTINGA>
2023-04-04 08:38:57 -0300

    Alguns updates

--- index.js
+++ index.js
@@ -8,10 +8,29 @@ app.listen(port, function(){
     console.log("Servidor no ar - Porta: 3000!")
 });
 
+pages =__dirname + '/public/views'
+
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/views/home.html');
+    res.sendFile(pages + '/home.html');
 });
 
+app.get('/home', function(req, res){
+    res.sendFile(pages + '/home.html');
+});
+
+
 app.get('/gameTest', function(req, res){
-    res.sendFile(__dirname + '/public/views/gameTest.html');
+    res.sendFile(pages + '/gameTest.html');
+});
+
+app.get('/cadastro', function(req, res){
+    res.sendFile(pages + '/cadastro.html');
+});
+
+app.get('/login', function(req, res){
+    res.sendFile(pages + '/login.html');
+});
+
+app.get('/cadastrados', function(req, res){
+    res.sendFile(pages + '/cadastrados.html');
 });

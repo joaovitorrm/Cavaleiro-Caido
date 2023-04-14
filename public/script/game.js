931a368807089568e9aa440ed4698512862d0f32
JOAO VITOR RODRIGUES MORRUDO <10160145@lab-409-18.RESTINGA>
2023-04-14 09:51:08 -0300

    Play button + algumas coisas

--- public/script/game.js
+++ public/script/game.js
@@ -1,95 +1,86 @@
-const canvas = document.getElementById('game')
+const play = document.getElementById('playButton');
+const canvas = document.getElementById('game');
+const ctx = game.getContext('2d');
 
-game.width = 1280
-game.height = 720
 
-const ctx = game.getContext('2d')
-
-const playerSprite = new Image()
-playerSprite.src = '../images/cavaleiro.png'
+// Criando o jogador
 
 class Player {
-    constructor(x, y, w, h) {
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        this.speed = 5
-        this.move = this.speed
-        this.direction = 'x'
+    constructor(x, y) {
+        this.playerSprite = new Image()
+        this.playerSprite.src = '../images/cavaleiro.png'
+        this.config = {
+            x: x,
+            y: y,
+            w: this.playerSprite.width,
+            h: this.playerSprite.height
+        }
     }
-
     draw() {
-        ctx.drawImage(playerSprite, this.x, this.y);
+        ctx.drawImage(this.playerSprite, this.config.x, this.config.y);
     }
-
     check_collision(){
-        if (this.x < 0 || this.x + this.w > game.width){
+        if (this.config.x < 0 || this.config.x + this.config.w > game.width){
             return 'x';
         } 
-        else if  (this.y < 0 || this.y + this.h > game.height){
+        else if  (this.config.y < 0 || this.config.y + this.config.h > game.height){
             return 'y';
         }
-    }
-
-    update(){
-        switch (this.direction){
-            case 'x':
-                this.x += this.move;
-                break
-            case 'y':
-                this.y += this.move;
-                break
-        }
+    }    
+    move(){ 
         
-        let colided = this.check_collision()        
-
-        switch (colided){
-            case 'x':
-                this.x -= this.move;
-                break
-            case 'y':
-                this.y -= this.move;
-                break   
-        }
     }
 }
 
-let player = new Player(20, 20, 40, 40)
 
-function animate(){
-    window.requestAnimationFrame(animate)
-    ctx.fillStyle = 'white'
-    ctx.fillRect(0, 0, game.width, game.height)
-    player.update()
-    player.draw()
-}    
-animate()
-
-document.addEventListener('keydown', (e) => {
-    switch (e.key){
-        case 'w':
-            player.direction = 'y'
-            player.move = -player.speed
-            break
-        case 'a':
-            player.direction = 'x'
-            player.move = -player.speed
-            break
-        case 's':
-            player.direction = 'y'
-            player.move = +player.speed
-            break
-        case 'd':
-            player.direction = 'x'
-            player.move = +player.speed
-            break
-        case 'p':
-            if (canvas.style.display == 'none'){
-                canvas.style.display = 'block'
-            } else {
-                canvas.style.display = 'none'
-            }
+class MainGame{
+    // Setup
+    constructor(){
+        game.width = 1280
+        game.height = 720        
     }
-})
+
+    // Rodar o jogo
+    animate(){
+        
+    }
+    run(){
+    let player = new Player(20, 20)
+    function animate(){
+        window.requestAnimationFrame(animate)
+        ctx.fillStyle = 'white'
+        ctx.fillRect(0, 0, game.width, game.height)
+        player.move()
+        player.draw()
+    }
+    
+    animate()
+
+    document.addEventListener('keydown', (e) => {
+        switch (e.key){
+            case 'w':
+                player.config.y -= 5
+                break;
+            case 'a':
+                player.config.x -= 5
+                break;
+            case 's':
+                player.config.y += 5
+                break;
+            case 'd':
+                player.config.x += 5
+                break;
+        }
+    })
+}
+}
+
+
+play.onclick = () => {
+    play.style.display = 'none'
+    canvas.style.display = 'block'
+    const start = new MainGame()
+    start.run()
+}
+
 

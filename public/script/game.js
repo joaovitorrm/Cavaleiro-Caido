8630890cb8d1c554a8e68a71730c0390dcb1c2e9
Filipe Zatta <filipe.fbz@gmail.com>
2023-04-14 22:16:00 -0300

    add comentarios no codigo, forma de aumentar o tamanho do personagem e mudança de cores

--- public/script/game.js
+++ public/script/game.js
@@ -1,61 +1,80 @@
+//pega o botão
 const play = document.getElementById('playButton');
+//pega o canvas
 const canvas = document.getElementById('game');
+//cria o coiso 2d da API canvas
 const ctx = game.getContext('2d');
 
 
-// Criando o jogador
+// Classe do jogador
 
 class Player {
     constructor(x, y) {
-        this.playerSprite = new Image()
-        this.playerSprite.src = '../images/cavaleiro.png'
+        this.playerSprite = new Image();
+        this.playerSprite.onload = () => {
+            //para alterar o tamanho do personagem é só mexer aqui, para deixar padrão é só deixar '*1'
+            this.config.w = this.playerSprite.width * 1.5;
+            this.config.h = this.playerSprite.height * 1.5;
+        };
+        this.playerSprite.src = '../images/cavaleiro.png';
         this.config = {
             x: x,
             y: y,
-            w: this.playerSprite.width,
-            h: this.playerSprite.height
-        }
+            w: 0,
+            h: 0,
+        };
     }
+    
+    //método que desenha o personagem, ele recebe 5 valores: imagem, origem x, origem y, tamanho w, largura h
     draw() {
-        ctx.drawImage(this.playerSprite, this.config.x, this.config.y);
+        ctx.drawImage(this.playerSprite, this.config.x, this.config.y, this.config.w, this.config.h);
     }
-    check_collision(){
+    //implementar colisão
+    check_collision() {
         if (this.config.x < 0 || this.config.x + this.config.w > game.width){
             return 'x';
         } 
-        else if  (this.config.y < 0 || this.config.y + this.config.h > game.height){
+        else if (this.config.y < 0 || this.config.y + this.config.h > game.height){
             return 'y';
         }
     }    
-    move(){ 
+    //?
+    move() {
         
     }
 }
 
-
+//Classe que roda o jogo
 class MainGame{
     // Setup
     constructor(){
         game.width = 1280
-        game.height = 720        
+        game.height = 720
     }
 
-    // Rodar o jogo
+    
+    //??
     animate(){
         
     }
+
+    // Rodar o jogo
     run(){
-    let player = new Player(20, 20)
+        //classe player recebe metade do tamanho x e metade do y para posicionar o personagem no exato centro
+    let player = new Player(game.width/2, game.height/2)
     function animate(){
         window.requestAnimationFrame(animate)
-        ctx.fillStyle = 'white'
+        //cor background do canvas
+        ctx.fillStyle = '#8b6f41'
+        //contexto da API do canvas; CTX
         ctx.fillRect(0, 0, game.width, game.height)
         player.move()
         player.draw()
     }
-    
+    //roda o método animate (gera o jogo)
     animate()
 
+    //leitor de inputs para movimento
     document.addEventListener('keydown', (e) => {
         switch (e.key){
             case 'w':
@@ -75,10 +94,16 @@ class MainGame{
 }
 }
 
-
+//ação do botão html
 play.onclick = () => {
+    //remove o botão 
     play.style.display = 'none'
+    //faz o canvas ser visível
     canvas.style.display = 'block'
+    //centraliza o canvas
+    canvas.style.marginTop = '-20%'
+
+    //inicia o jogo
     const start = new MainGame()
     start.run()
 }

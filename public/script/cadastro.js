a68f31507b2521b172259a71ccb374b6b42125ac
joao <joao-morrudo@hotmail.com.br>
2023-05-25 15:38:27 -0300

    finalização estilo tela cadastro

---
+++ public/script/cadastro.js
@@ -0,0 +1,12 @@
+
+document.getElementById('imgPerfil').addEventListener('change', function(e) {
+    if (e.target.files[0]) {
+      let foto = new FileReader()
+      foto.onload = () => {
+        document.getElementById('img').src = foto.result;        
+      }
+      foto.readAsDataURL(e.target.files[0])
+    } else {
+        document.getElementById('img').src = '../images/icones/perfil.png';  
+    }
+});

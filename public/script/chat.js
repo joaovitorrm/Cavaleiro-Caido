db9ed6d9f58e32bbaafc517e1944a34056d64107
joao <joao-morrudo@hotmail.com.br>
2023-05-28 10:43:08 -0300

    anotações chat.js

--- public/script/chat.js
+++ public/script/chat.js
@@ -1,3 +1,5 @@
+
+// abre o chat
 document.getElementById('chat').onclick = () => {
     const menu = document.getElementById('chat-menu');
     if (menu.style.display == "none" || menu.style.display == ''){
@@ -7,9 +9,11 @@ document.getElementById('chat').onclick = () => {
     }    
 }
 
+// pega o textarea e adiciona um evento que vigia as teclas pressionadas
 const texto = document.getElementById('texto')
 texto.addEventListener('keydown', sendMessage)
 
+// função que adiciona o texto na tela e limpa a caixa de texto
 function addTextChat(){    
     document.getElementById('conversa').innerHTML += 
     "<div class='remetente'>" +
@@ -18,17 +22,20 @@ function addTextChat(){
     texto.value = '';
 }
 
+// checa se o botão de enviar mensagem foi pressionado
 document.getElementById('enviar-mensagem').onclick = () => {
     if (texto.value != ''){
         addTextChat();
     }
 }
 
+// checa se a tecla pressionada no textarea é a tecla enter
 function sendMessage(e){    
     if (e.which == 13){
         if (texto.value != ''){        
             addTextChat();            
         }
+        // impede que o usuario desça de linha
         e.preventDefault();            
     }
 }

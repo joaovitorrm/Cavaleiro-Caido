44ed8868a8f6ec8890d4ef001fe5e33eddda8d97
Filipe Zatta <filipe.fbz@gmail.com>
2023-06-30 09:06:21 -0300

    add linguas ásia/oriente

--- public/script/paginas/tradutor.js
+++ public/script/paginas/tradutor.js
@@ -1,5 +1,11 @@
 import portugues from '../../idiomas/portugues.json' assert {type: "json"}
 import english from '../../idiomas/english.json' assert {type: "json"}
+import francais from '../../idiomas/francais.json' assert {type: "json"}
+import deutsch from '../../idiomas/deutsch.json' assert {type: "json"}
+import hebrew from '../../idiomas/hebrew.json' assert {type: "json"}
+import simp_chinese from '../../idiomas/simp_chinese.json' assert {type: "json"}
+import greek from '../../idiomas/greek.json' assert {type: "json"}
+import irish from '../../idiomas/irish.json' assert {type:"json"}
 
 let spans = document.querySelectorAll("span");
 let idioma = document.getElementById("idiomas");

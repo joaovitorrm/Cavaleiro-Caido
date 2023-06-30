a06577772c88312123856d133dea9fe01d3d0671
Filipe Zatta <filipe.fbz@gmail.com>
2023-06-30 08:01:09 -0300

    feito tradutor

--- public/script/paginas/tradutor.js
+++ public/script/paginas/tradutor.js
@@ -1,20 +1,24 @@
+import portugues from '../../idiomas/portugues.json' assert {type: "json"}
 import english from '../../idiomas/english.json' assert {type: "json"}
 
 let spans = document.querySelectorAll("span");
+let idioma = document.getElementById("idiomas");
 
-for (const divId in english) {
-    let divs = document.getElementById(divId);
-    for (let span of spans) {
-        let c = 0
-        for (let div of divs.children) {
-            if (div.querySelector("#" + span.id)) {
-                span.innerText = english[divId][c]
+
+
+function translate(language){
+    for (const divId in language) {
+        let divs = document.getElementById(divId);
+        for (let span of spans) {
+            let c = 0
+            for (let div of divs.children) {
+                if (div.querySelector("#" + span.id)) {
+                    span.innerText = language[divId][c]
+                }
+                c++
             }
-            c++
         }
     }
 }
 
-
-
-
+idioma.addEventListener("change", () => translate(eval(idioma.value)))
